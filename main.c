3290da7f4fc216801835517480e065b8b242e628
Daniel Montanari <dmontanari@gmail.com>
2015-02-04 15:59:02 -0200

    Added example of lcd_print_position

--- main.c
+++ main.c
@@ -24,6 +24,10 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include <lcd_hd44780u.h>
 
@@ -47,13 +51,21 @@ int main (void)
         fprintf( stdout, "Printing some text.........."), fflush( stdout );
 
 ///                 12345678901234567890
-        lcd_print( "RPi I2C LCD LIBRARY\0", 1 );
-        lcd_print( "github.com/dmontanari/librpilcd\0", 2 );
-
+        lcd_print( "RPi I2C LCD LIBRARY", 1 );
+        lcd_print( "http://goo.gl/fGLxqw", 2 );
         fprintf( stdout, "DONE\n" ), fflush( stdout );
         fprintf( stdout, "Sleeping for 5 seconds......"), fflush( stdout );
         sleep ( 5 );
         fprintf( stdout, "DONE\n" ), fflush( stdout );
+        fprintf( stdout, "Counting....................");
+        lcd_print( "12345678901234567890", 1 ), fflush( stdout );
+        lcd_print( "abcdefghijklmnopqrst", 2 ), fflush( stdout );
+        fprintf( stdout, "DONE\n" ), fflush( stdout );
+        fprintf( stdout, "Sleeping for 5 seconds......"), fflush( stdout );
+        sleep ( 5 );
+        fprintf( stdout, "DONE\n" ), fflush( stdout );
+
+
         fprintf( stdout, "Blinking 5 times.."), fflush( stdout );
 
         /// "Blink" a few times
@@ -68,20 +80,20 @@ int main (void)
         fprintf( stdout, "DONE\n" ), fflush( stdout );
 
         lcd_clear( );
-        fprintf( stdout, "Testing shift to left.......");
-        lcd_print( "shift left", 1 );
+        fprintf( stdout, "Shifting some text..........");
+        lcd_print( "shift", 1 );
 
-        for ( i = 0; i < 10; i++ ) {
+        for ( i=0;i<15;i++ ) {
+                lcd_scroll_right( );
+                usleep( 250000 );
+        }
 
+        for ( i = 0; i < 15; i++ ) {
                 lcd_scroll_left( );
                 usleep( 250000 );
         }
         fprintf( stdout, "DONE\n" );
-        fprintf( stdout, "Testing shift to right......");
-        for ( i=0;i<10;i++ ) {
-                lcd_scroll_right( );
-                usleep( 250000 );
-        }
+
         fprintf( stdout, "DONE\n" );
         fprintf( stdout, "Sleeping for 5 seconds......"), fflush( stdout );
         sleep( 5 );
@@ -91,6 +103,33 @@ int main (void)
         fprintf( stdout, "DONE\n"), fflush( stdout );
         fprintf( stdout, "Test finishedt\n" ), fflush( stdout );
 
+        /// Show core temperature
+        /// $ cat /sys/class/thermal/thermal_zone0/temp
+        /// 47078
+        ///
+        char temperature[6];
+        int fd;
+
+        fd = open( "/sys/class/thermal/thermal_zone0/temp", O_RDONLY );
+        if ( fd == -1 ) {
+
+                fprintf(stderr, "Open temperature sensor fail %s\n", strerror( errno ) );
+                lcd_close();
+                exit( EXIT_FAILURE );
+
+        }
+        temperature[5] = 0;     /// set NTZ
+
+        if ( read( fd, temperature, 5 ) == -1 ) {
+
+                fprintf(stderr, "Read temperature sensor fail %s\n", strerror( errno ) );
+                lcd_close();
+                close(fd);
+                exit( EXIT_FAILURE );
+
+        }
+        lcd_print( "Temperature", 1 );  /// 11 chars
+        lcd_print_position( temperature, 1, 12 );
 
         lcd_close();
 
